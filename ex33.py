13b51cfbded336484e3b6d9d03e3a5c9076f63f4
espinolaramd <espinolaramd@hartwick.edu>
2019-10-14 11:20:36 -0400

    update

--- ex33.py
+++ ex33.py
@@ -1,3 +1,18 @@
 #Diego Espinola
 #13.10.19
 #While Loops
+
+i=0
+numbers=[]
+
+while i<6:
+    print(f"A the top i is {i}")
+    numbers.append(i)
+
+    i= i+1
+    print("Numbers now: ", numbers)
+    print(f"At tge bottom i is {i}")
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
